663d2790cb1b635cc4d7756586a89fd32731c5b4
abdulh70 <abdulazizh70@gmail.com>
2024-10-15 11:14:30 +0300

    objects lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "abdul"
+}
+function updateEmployeeWithKeyAndValue(empl, key, value) {
+    //create a copy to update destructively
+    const newOb= {...empl};
+
+    // assign a new value
+    newOb[key] = value;
+
+    return newOb;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(empl, key, value) {
+    empl[key] = value;
+    return empl;
+  }
+  function deleteFromEmployeeByKey(empl, key) {
+    const newOb = { ...employee };
+    delete newOb[key];
+    return newOb;
+  }
+  function destructivelyDeleteFromEmployeeByKey(empl, key) {
+    delete empl[key];
+    return empl;
+  }
